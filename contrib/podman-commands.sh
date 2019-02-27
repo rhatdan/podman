e28522fdbd8b9fe924b244fb09af464fb4da4494
Ed Santiago <santiago@redhat.com>
2019-02-27 12:20:09 -0700

    DO NOT MERGE: expansion of dwalsh's comparison script
    Something that compares _all_ subcommands. It's a horrible
    mess because some man pages display COMMANDS and some
    display SUBCOMMANDS; and some show the subcommand name
    in the first column, others show the podman-xxx man page;
    and there are inconsistencies galore.
    
    This is a fake PR so Dan can look at the rest of the
    inconsistencies that his script didn't look at
    
    Signed-off-by: Ed Santiago <santiago@redhat.com>

--- contrib/podman-commands.sh
+++ contrib/podman-commands.sh
@@ -1,15 +1,57 @@
-#!/bin/sh
-podman --help | sed -n -Ee '0,/Available Commands/d' -e '/^Flags/q;p' | sed '/^$/d' | awk '{ print $1 }' > /tmp/podman.cmd
-man podman | sed -n -e '0,/COMMANDS/d' -e '/^FILES/q;p' | grep podman | cut -f2 -d- | cut -f1 -d\( > /tmp/podman.man
-echo diff -B -u /tmp/podman.cmd /tmp/podman.man
-diff -B -u /tmp/podman.cmd /tmp/podman.man
+#!/bin/bash
 
-podman image --help | sed -n -e '0,/Available Commands/d' -e '/^Flags/q;p' | sed '/^$/d' | awk '{ print $1 }' > /tmp/podman-image.cmd
-man podman image | sed -n -Ee '0,/COMMANDS/d'  -e 's/^[[:space:]]*//' -e '/^SEE ALSO/q;p' | grep podman | cut -f1 -d' ' | sed 's/^.//' > /tmp/podman-image.man
-echo diff -B -u /tmp/podman-image.cmd /tmp/podman-image.man
-diff -B -u /tmp/podman-image.cmd /tmp/podman-image.man
+# override with, e.g.,  PODMAN=./bin/podman
+PODMAN=${PODMAN:-podman}
 
-podman container --help | sed -n -e '0,/Available Commands/d' -e '/^Flags/q;p' | sed '/^$/d' | awk '{ print $1 }' > /tmp/podman-container.cmd
-man podman container | sed -n -Ee '0,/COMMANDS/d'  -e 's/^[[:space:]]*//' -e '/^SEE ALSO/q;p' | grep podman | cut -f1 -d' ' | sed 's/^.//' > /tmp/podman-container.man
-echo diff -B -u /tmp/podman-container.cmd /tmp/podman-container.man
-diff -B -u /tmp/podman-container.cmd /tmp/podman-container.man
+function die() {
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+
+function podman_commands() {
+    $PODMAN help "$@" |\
+        awk '/^Available Commands:/{ok=1;next}/^Flags:/{ok=0}ok { print $1 }' |\
+        grep .
+}
+
+function podman_man() {
+    # - tr: remove nonalphanumeric and nonwhitespace characters
+    # - awk: print everything from COMMANDS or SUBCOMMANDS to next section
+    # - egrep: print the first or second column, matching podman-xxxxx
+    # - sed: strip away all except the last word, and remove the '(1)'
+    man ./docs/$1.1 |\
+        tr -cd 'a-zA-Z0-9 \012-' |\
+        awk '/^(SUB)?COMMANDS/{ok=1;next}/^[^ ]/{ok=0}ok' |\
+        egrep -o 'podman-[a-z0-9-]+' |\
+        sed -e 's/^.*-//' -e 's/1$//'
+}
+
+function compare_help_and_man() {
+    echo
+    echo "checking: $@"
+
+    # e.g. podman, podman-image, podman-volume
+    basename=$(echo podman "$@" | sed -e 's/ /-/g')
+
+    # uniq is necessary because, e.g., in podman-image.1 the 'list' and 'ls'
+    # commands reference 'podman-images(1)' (?!?)
+    podman_commands "$@" | sort | uniq > /tmp/${basename}_help.txt
+    podman_man $basename | sort | uniq > /tmp/${basename}_man.txt
+
+    diff -u /tmp/${basename}_help.txt /tmp/${basename}_man.txt
+
+    # Now look for subcommands, e.g. container, image
+    for cmd in $(< /tmp/${basename}_help.txt); do
+        # FIXME FIXME FIXME
+        usage=$($PODMAN "$@" $cmd --help | grep -A2 '^Usage:' | grep . | tail -1)
+
+        # if ends in '[command]', recurse into subcommands
+        if expr "$usage" : '.*\[command\]$' >/dev/null; then
+            compare_help_and_man "$@" $cmd
+        fi
+    done
+}
+
+
+compare_help_and_man
